29090645294f3fef4338a4ac7b82db08df906052
Ph4ntonMasters <marcioaugusto62@gmail.com>
2024-04-23 12:54:49 -0300

    merge

--- Exercicio_API/Exercicio_API/obj/Debug/net8.0/Exercicio_API.AssemblyInfo.cs
+++ Exercicio_API/Exercicio_API/obj/Debug/net8.0/Exercicio_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+326e69f8207f6a76e02a0dbcf03f969776b62005")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
